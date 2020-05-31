051eabe99897b5d676ea7c6c53646572f85df6a9
Dr Nic Williams <drnicwilliams@gmail.com>
2020-06-01 09:44:04 +1000

    script to discover what 'aws --version' is in blob

---
+++ awscliv2-blob-version.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd $ROOT
+
+rm -rf tmp/awscli
+mkdir tmp/awscli
+unzip -qq -d tmp/awscli blobs/awscliv2/awscliv2.zip
+
+docker run -ti -v $PWD/tmp/awscli:/awscli ubuntu /awscli/aws/dist/aws --version
+# rm -rf tmp/awscli
