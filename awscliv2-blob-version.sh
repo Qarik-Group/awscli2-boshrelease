d6630b3329ebe4c02aa820d8baefb79ed5812d0c
Bi Bamba <maxime-tizie@hotmail.fr>
2022-09-27 11:37:08 -0500

    correct syntax

--- awscliv2-blob-version.sh
+++ awscliv2-blob-version.sh
@@ -4,7 +4,7 @@ ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $ROOT
 
 rm -rf tmp/awscli
-mkdir tmp/awscli
+mkdir -p tmp/awscli
 unzip -qq -d tmp/awscli blobs/awscliv2/awscliv2.zip
 
 docker run -ti -v $PWD/tmp/awscli:/awscli ubuntu /awscli/aws/dist/aws --version
